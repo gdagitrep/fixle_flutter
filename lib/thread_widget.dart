9c0798c3bfa5c878dac1356e0c665aba2c0fc6a8
Dhirendra Singh <dhirendra.12.singh@live.in>
2023-04-14 02:15:57 -0700

    Update comment box; removed firebase dependencies
    published 0.0.2

--- lib/thread_widget.dart
+++ lib/thread_widget.dart
@@ -1,6 +1,7 @@
 import 'package:fixle_feedback_flutter/primitive_wrapper.dart';
 import 'package:fixle_feedback_flutter/project_and_threads_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ThreadWidget extends StatefulWidget {
   final ThreadData threadData;
@@ -31,90 +32,149 @@ class _ThreadWidgetState extends State<ThreadWidget> {
   }
 
   Widget commentThread(List<Comment> previousComments, PrimitiveWrapper<Offset>? threadPosition) {
-    Widget bottomBar() => SizedBox(
-        height: 35,
-        child: Row(
-          children: [
-            // if no comments, show cross button, or show minimize button
-            IconButton(
-              onPressed: () {
-                widget.threadMinimizingCallback();
-                widget.makeFixleOverlayVisible();
-              },
-              icon: Icon(
-                previousComments.isEmpty ? Icons.close : Icons.minimize,
-                color: Colors.blue,
-              ),
-              padding: EdgeInsets.zero,
-            ),
-            Expanded(child: Container()),
-            IconButton(
-              onPressed: () {
-                final form = formKey.currentState!;
-                if (form.validate()) {
-                  form.save();
-                }
-              },
-              icon: const Icon(
-                Icons.arrow_circle_right_outlined,
-                color: Colors.blue,
-              ),
-              padding: EdgeInsets.zero,
-            )
-          ],
-        ));
     List<Widget> children = <Widget>[];
     for (var comment in previousComments) {
-      children.add(Text(comment.commentText));
+      children.add(singleComment(comment.commentText));
     }
-    children.add(TextFormField(
-      controller: fieldText,
-      textInputAction: TextInputAction.next,
-      textCapitalization: TextCapitalization.sentences,
-      keyboardType: TextInputType.multiline,
-      maxLines: 10,
-      minLines: 1,
-      decoration: const InputDecoration(
-        hintText: 'Add comment',
-      ),
-      validator: (val) {
-        if (val == null || val.isEmpty) {
-          return 'Enter comment';
-        }
-        return null;
-      },
-      onSaved: (val) {
-        previousComments.add(Comment()
-          ..commentText = val!
-          ..commentorEmail = "yetToFill");
-        fieldText.clear();
-        widget.threadData.saveThreadData(widget.projectId);
-        setState(() {
-          placeIsLocked = true;
-        });
-        // printThreadData(widget.threadData);
-      },
-    ));
-    children.add(bottomBar());
-
-    return Positioned(
-        left: threadPosition?.value.dx,
-        top: threadPosition?.value.dy,
-        child: GestureDetector(
-            onPanUpdate: (details) {
-              if (placeIsLocked == false) {
-                setState(() {
-                  threadPosition?.value += details.delta;
-                });
+    children.add(const SizedBox(height: 5,));
+    children.add(addCommentBox(previousComments));
+    children.addAll([
+      const Divider(),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              widget.threadMinimizingCallback();
+              widget.makeFixleOverlayVisible();
+            },
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: Icon(
+                previousComments.isEmpty
+                    ? Icons.close
+                    : Icons.minimize,
+                color: const Color(0xFF2C384A),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              final form = formKey.currentState!;
+              if (form.validate()) {
+                form.save();
               }
             },
-            child: Material(
-                elevation: 10,
+            child: const MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: Icon(
+                Icons.arrow_circle_right_outlined,
+                color: Color(0xFF2C384A),
+              ),
+            ),
+          ),
+        ],
+      )
+    ]);
+    return Positioned(
+      left: threadPosition?.value.dx,
+      top: threadPosition?.value.dy,
+      child: GestureDetector(
+        onPanUpdate: (details) {
+          if (placeIsLocked == false) {
+            setState(() {
+              threadPosition?.value += details.delta;
+            });
+          }
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Material(
+            elevation: 10,
+            child: Form(
+              key: formKey,
+              child: Container(
+                padding: const EdgeInsets.all(6.0),
+                width: 250.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFE7EEFB),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: children,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget singleComment(String comment) {
+    return RichText(
+        text: TextSpan(children: [
+      WidgetSpan(
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
                 child: SizedBox(
-                    width: 200,
-                    child: Padding(
-                        padding: const EdgeInsets.fromLTRB(5, 0, 10, 0),
-                        child: Form(key: formKey, child: Column(children: children)))))));
+                    height: 20.0,
+                    width: 20.0,
+                    child: Image.network(
+                        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                        fit: BoxFit.cover)))),
+      ),
+      TextSpan(
+        text: comment,
+        style: const TextStyle(fontSize: 14, color: Colors.black),
+      )
+    ]));
+  }
+
+  Widget addCommentBox(List<Comment> previousComments) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
+      height: 30,
+      decoration: BoxDecoration(
+        color: const Color(0xFFcfd7e2),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: TextFormField(
+        controller: fieldText,
+        textInputAction: TextInputAction.next,
+        textCapitalization: TextCapitalization.sentences,
+        keyboardType: TextInputType.multiline,
+        maxLines: 10,
+        minLines: 1,
+        style: const TextStyle(fontSize: 14, color: Colors.black),
+        decoration: const InputDecoration(
+          hintText: 'Add comment',
+          border: InputBorder.none,
+        ),
+        validator: (val) {
+          if (val == null || val.isEmpty) {
+            return 'Enter comment';
+          }
+          return null;
+        },
+        onSaved: (val) {
+          previousComments.add(Comment()
+            ..commentText = val!
+            ..commentorEmail = "yetToFill");
+          fieldText.clear();
+          widget.threadData.saveThreadData(widget.projectId);
+          setState(() {
+            placeIsLocked = true;
+          });
+        },
+      ),
+    );
   }
 
   Widget minimizedThreadAndShowCross() {
